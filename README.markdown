e756c971b49a361e73ace7771059c93657ce75ee
Jaime Cruz <jhcruz@zoho.com>
2012-01-10 14:19:02 -0600

    initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Aplicacion para realizar las encuestas de satisfacción a clientes.
+
+Evaluacion del seguimiento a un incidente por parte del cliente.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SatisfactionSurvey::Application.config.secret_token = 'b6c2253f60c3ec00530d208e2b612a2bed433d5559bc05d27b941f8e29e813ade14827fb5c90597c4162e4d645d5db4ff5031a78e6bb3e455f3ff9926e425682'
